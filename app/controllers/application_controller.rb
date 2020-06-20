80ebb438d3e8804b7b36b6192f16599534e70490
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-20 20:55:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
     recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect to "/recipes/#{recipe.id}"
   end
-  
+
   patch '/recipes/:id' do
     recipe = Recipe.find_by_id(params[:id])
     recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
